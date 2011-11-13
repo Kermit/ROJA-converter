81bb2ba98ef5e5ff624d712b28094d91566ff52e
Maciej Płaza <plaza.maciej@gmail.com>
2011-11-13 21:47:53 +0100

    Main file
    Signed-off-by: Maciej Płaza <plaza.maciej@gmail.com>

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <QtCore/QCoreApplication>
+
+#include <iostream>
+
+#include "config.h"
+#include "chmfile.h"
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+    cout << "Witaj w konwerterze RoJa. Program ten zaraz pobierze plik CHM z sieci lub Twojego dysku twardego";
+    cout << " i przekonwertuje do bazy danych SQLite." << endl;
+    cout << endl;
+
+    Config::checkConfig();
+
+    CHMFile chmFile;
+    chmFile.convertCHM();
+    return a.exec();
+}
